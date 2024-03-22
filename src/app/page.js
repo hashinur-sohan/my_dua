b93684bc96bf0a00cbe72f49cbdf6c2ff56c1e22
Hashinur Sohan <hashinur.sohan@outlook.com>
2024-03-23 05:09:12 +0600

    left bar started

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main> 
+      {/* Container */}
+      <div className="bg-[#F7F8FA] ">
+            {/*Left bar */}
+            <div className=" flex flex-col container h-[927] w-[100px] mt-[40px] ml-[40px] border-1 color rounded-3xl bg-white ">
+                <h2></h2>
+            </div>
+      </div>
     </main>
   );
 }
