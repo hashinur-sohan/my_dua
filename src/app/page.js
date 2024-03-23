b1b8e916e4e426f4d68c23da8ac7a63dfe8b1a34
Hashinur Sohan <hashinur.sohan@outlook.com>
2024-03-23 15:18:26 +0600

    setting part started

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <main> 
       {/* Container */}
-      <div className="bg-[#F7F8FA] ">
+      <div className=" relative  gap-5 flex flex-row bg-[#F7F8FA]  ">
             {/*Left bar */}
-            <div className="flex flex-col container h-[927] w-[100px] mt-[40px] ml-[40px] border-1 color rounded-3xl bg-white ">
+            <div className="flex flex-col container h-[927] w-[100px] mt-[40px] ml- [40px] border-1 color rounded-3xl bg-white ">
                 <a href="/"><Image src="/dua_logo.svg" alt="dua" width={73} height={73}/></a>
                 <a href="/"><Image src="/Frame 545.svg" alt="Frame" width={38} height={38} className=" mt-[80px] mb-[75px] ml-[20px]"/></a>
                 <a href="/"><Image src="/I_want_to_support_logo.svg" alt="Want to support" width={57} height={56} className=" ml-[10px]"/></a>
@@ -24,6 +24,9 @@ export default function Home() {
                 {/* Account */}
               <div>
                 <a href="/"><Image src="/account_logo.svg" alt="AccountLogo" width={67.5} height={45} className=" mt-[80px] mb-[75px] ml-[250px]  "/></a>
+              </div>
+              <div>
+                <a href="/"><Image src="/setting.svg" alt="Setting" width={67.5} height={45} className=" mt-[80px] mb-[75px] ml-[250px]  "/></a>
               </div>
            </div>
       </div>
