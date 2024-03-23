d6de189b8948e0972103e1c37df8a328bb9b8ddf
Hashinur Sohan <hashinur.sohan@outlook.com>
2024-03-23 12:39:38 +0600

    Account setup started

--- src/app/page.js
+++ src/app/page.js
@@ -13,13 +13,19 @@ export default function Home() {
                 <a href="/"><Image src="/Frame 545.svg" alt="Frame" width={38} height={38} className=" mt-[80px] mb-[75px] ml-[20px]"/></a>
                 <a href="/"><Image src="/I_want_to_support_logo.svg" alt="Want to support" width={57} height={56} className=" ml-[10px]"/></a>
             </div>
-            {/* Dua Page & Search Bar */}
+            
             <div className="flex flex-row justify-between">
-              <div className="flex flex-auto items-center">
+              {/* Dua Page & Search Bar */}
+              <div className="flex flex-auto items-center justify-between">
                 <h1 className="font-[Poppins] font-bold text-[24px]">Dua Page</h1>
+
+                <a href="/"><Image src="/search_bar.svg" alt="SearchBar" width={371} height={52} className=" mt-[80px] mb-[75px] ml-[20px]"/></a>
               </div>
-              <div><a href="/"><Image src="/search_bar.svg" alt="Frame" width={371} height={52} className=" mt-[80px] mb-[75px] ml-[20px]"/></a></div>
+           
+            <div>
+              <a href="/"><Image src="/account_logo.svg" alt="AccountLogo" width={67.5} height={45} className=" mt-[80px] mb-[75px] ml-[250px]  "/></a>
             </div>
+           </div>
       </div>
     </main>
   );
